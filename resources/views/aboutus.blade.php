7a73ac30409a4dd3be47657802807d3b5fb2e576
Ayuapry <ayuapriyanti0304@gmail.com>
2024-01-12 10:06:30 +0700

    feat:our-team image

--- resources/views/aboutus.blade.php
+++ resources/views/aboutus.blade.php
@@ -52,7 +52,7 @@
                     <!-- Row -->
                     <div class="row">
                         <div class="col-md-12 text-center">
-                            <img src="https://www.wrappixel.com/demos/ui-kit/wrapkit/assets/images/team/t1.jpg"
+                            <img src="{{ asset('css/Ayuapry.jpg') }}" alt="wrapkit" class="img-fluid rounded-circle"
                                 alt="wrapkit" class="img-fluid rounded-circle"
                                 style="object-fit: cover; width: 360px; height: 345px;" />
                         </div>
@@ -60,8 +60,7 @@
                             <div class="pt-2">
                                 <h5 class="mt-4 font-weight-medium mb-0">Ayu</h5>
                                 <h6 class="subtitle mb-3">All Role</h6>
-                                <p>You can relay on our amazing features list and also our customer services will be great
-                                    experience.</p>
+                                <p>Jangan Lupa ngerjain skripsi!</p>
                                 <ul class="list-inline">
                                     <li class="list-inline-item"><a href="#"
                                             class="text-decoration-none d-block px-1"><i
